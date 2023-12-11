296aed7d6e9270ec5cdbb453f71478ac8748cf50
Garrett Andrews <garrettjandrews@gmail.com>
2023-12-11 05:43:00 -0500

    day 11 part 1 (gotta go to work will finish later

---
+++ 2023/day11/Part1.java
@@ -0,0 +1,124 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Part1 {
+  public static void main(String[] args) {
+    if (args.length != 1) {
+      System.out.println("Usage: <input_filepath");
+    }
+
+    String input_filepath = args[0];
+    BufferedReader br;
+    ArrayList<String> lines = new ArrayList<>();
+
+    try {
+      br = new BufferedReader(new FileReader(input_filepath));
+      String line;
+      while ((line = br.readLine()) != null) {
+        lines.add(line);
+      }
+    } catch (IOException e) {
+      System.out.println(e);
+      return;
+    }
+
+    Galaxy galaxy = new Galaxy(lines);
+    galaxy.printImage();
+    galaxy.printGalaxyLocations();
+
+    System.out.println(galaxy.sumOfPairwiseDistances());
+  }
+}
+
+class Galaxy {
+  public ArrayList<String> image;
+  public ArrayList<int[]> galaxies;
+
+  public Galaxy(ArrayList<String> lines) {
+    // figure out where to expand rows
+    ArrayList<Integer> expansionRows = new ArrayList<>();
+    ArrayList<Integer> expansionCols = new ArrayList<>();
+    for (int i = 0; i < lines.get(0).length(); i++) {
+      expansionCols.add(i);
+    }
+
+    for (int row = 0; row < lines.size(); row++) {
+      String line = lines.get(row);
+      if (-1 == line.indexOf("#")) {
+        expansionRows.add(row);
+      }
+
+      for (int col = 0; col < line.length(); col++) {
+        if (line.charAt(col) == '#') {
+          expansionCols.remove(Integer.valueOf(col));
+        }
+      }
+    }
+
+    image = new ArrayList<>();
+    for (int row = 0; row < lines.size(); row++) {
+      String line = lines.get(row);
+      String lineToAdd = "";
+      for (int col = 0; col < line.length(); col++) {
+        lineToAdd += Character.toString(line.charAt(col));
+        if (expansionCols.contains(col)) {
+          lineToAdd += ".";
+        }
+      }
+      image.add(lineToAdd);
+      if (expansionRows.contains(row)) {
+        String blankLine = "";
+        for (int i = 0; i < lineToAdd.length(); i++) {
+          blankLine += ".";
+        }
+        image.add(blankLine);
+      }
+    }
+
+    // also going to build the list of galaxies
+    galaxies = new ArrayList<>();
+    for (int row = 0; row < image.size(); row++) {
+      String line = image.get(row);
+      for (int col = 0; col < line.length(); col++) {
+        if ('#' == line.charAt(col)) {
+          galaxies.add(new int[] { row, col });
+        }
+      }
+    }
+  }
+
+  public long sumOfPairwiseDistances() {
+    long res = 0;
+    for (int x = 0; x < galaxies.size() - 1; x++) {
+      for (int y = x + 1; y < galaxies.size(); y++) {
+        res += pairwiseDistance(galaxies.get(x), galaxies.get(y));
+      }
+    }
+    return res;
+  }
+
+  public long pairwiseDistance(int[] x, int[] y) {
+    long res1 = x[0] - y[0];
+    long res2 = x[1] - y[1];
+    if (res1 < 0) {
+      res1 *= -1;
+    }
+    if (res2 < 0) {
+      res2 *= -1;
+    }
+    return res1 + res2;
+  }
+
+  public void printImage() {
+    for (String line : image) {
+      System.out.println(line);
+    }
+  }
+
+  public void printGalaxyLocations() {
+    for (int[] position : galaxies) {
+      System.out.println(position[0] + "," + position[1]);
+    }
+  }
+}
